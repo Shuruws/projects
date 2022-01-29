47f24b344ccc717a564b304695e949c531274ff9
Rachelwsrbs <rachelwsrbs@gmail.com>
2022-01-29 11:53:39 -0500

    Second commit!

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-
 def main_wrapper():
-    print ("This is the start of our project")
+    print("This is the start of our project")
 
     print("this is the end of our python project")
+
 
 if __name__ == "__main__":
     main_wrapper()
